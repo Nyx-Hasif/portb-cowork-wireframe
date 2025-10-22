f3f2a9b41799c6290e513cba93ba17e0d7c22209
Nyx-Hasif <nikaresif97@gmail.com>
2025-10-23 01:15:10 +0800

    fixed text overwidth

--- components/Partnership.tsx
+++ components/Partnership.tsx
@@ -36,7 +36,7 @@ const Partnership = () => {
       {/* centered content */}
       <div className="max-w-7xl mx-auto px-6 md:px-10 py-20 space-y-16">
         {/* intro */}
-        <div className="text-center space-y-4 border border-black max-w-3xl mx-auto p-4">
+        <div className="text-center space-y-4  max-w-3xl mx-auto p-4">
           <h2 className="text-4xl md:text-5xl font-extrabold tracking-tight">
             Building Capacity &amp; Vibrant Communities
           </h2>
